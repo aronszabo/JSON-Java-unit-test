e79681e098a35eb4efc51c1ed2cb0acccc25d851
Aron Szabo <aron@szaboaron.hu>
2017-10-02 12:10:06 +0200

    Testing

--- test/java/org/json/junit/JSONPointerTest.java
+++ test/java/org/json/junit/JSONPointerTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import org.json.JSONArray;
